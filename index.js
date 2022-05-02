cb6fcbb28477a94057fa6f4037fb1824063d8bc7
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-02 16:56:01 -0700

    commit tree 9dda1a87c3d2cbf43c488167dc6b422833c22a29

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const newArray = [];
+    for (let i = 0; i<names.length ;i++){
+        newArray[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(integer){
+    while(integer >= 0){
+        console.log(integer);
+        integer--;
+    }
+}
